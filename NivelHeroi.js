2541fab92255653ef3f64bee9d178ba343cf2f3c
fpelvison <gtellviison@outlook.com>
2026-09-03 14:00:49 -0300

    Desafio  finalizado

---
+++ NivelHeroi.js
@@ -0,0 +1,41 @@
+var heroi = "Alistair"
+var XP = 113000
+var nivel = ""
+
+if ((XP <= 1000)  ){
+  
+    nivel = "Ferro"
+    
+
+}else if (XP <= 2000 ){
+
+    nivel = "Bronze"
+
+}else if (XP <= 5000){
+
+    nivel = "Prata"
+
+}else if (XP <= 7000){
+
+    nivel = "Ouro"
+  
+}else if (XP <= 8000){
+
+    nivel = "Platina"
+    
+}else if (XP <= 9000){
+
+    nivel = "Ascendente"
+
+}else if (XP <= 10000 ){
+
+    nivel = "Imortal"
+
+}else{
+
+    nivel = "Radiante"
+
+
+}
+
+console.log ("O Herói de nome " + heroi +  " está no nível " + nivel)
